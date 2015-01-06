4edb8291ed97b954d283d0efed846b1e90c67da1
Ham5ter <ham5ter@ham5ter.de>
2015-01-06 21:25:22 +0100

    First Commit

---
+++ CAOS Console/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CAOS;
+
+namespace CAOS_Console
+{
+    class Program
+    {
+        static CaosInjector ci;
+        static string Version = "0.1.1";
+        static string Website = "http://Creatures.Ham5ter.de";
+        static string GameName;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ham5ter's CAOS Console Version " + Version + "\nFor Updates and more visit:" + Website + "\n");
+            bool initialized = false;
+            if (args.Length >= 1)
+            {
+                if (InitializeInjector(args[0]))
+                {
+                    Console.WriteLine("Game Name: \"" + args[0] + "\"");
+                    GameName = args[0];
+                    initialized = true;
+                }
+            }
+            else if (InitializeInjector("Docking Station"))
+            {
+                GameName = "Docking Station";
+                initialized = true;
+            }
+            else if (InitializeInjector("Creatures 3"))
+            {
+                GameName = "Creatures 3";
+                initialized = true;
+            }
+
+
+            while (initialized)
+            {
+                Console.Write(">>> ");
+                try
+                {
+                    Console.WriteLine(ci.ExecuteCaosGetResult(Console.ReadLine()).Content);
+                }
+                catch (Exception)
+                {
+                    initialized = false;
+                }
+            }
+
+            Console.WriteLine("Ooops... Something went wrong, is the Game Started?\n\n\tPress Any Key to Quit!");
+            Console.ReadKey(true);
+        }
+        static bool InitializeInjector(string GameName)
+        {
+            try
+            {
+                ci = new CaosInjector(GameName);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# README #
+
+Caos Console is a litle Windows Tool for "Creatures 3" and "Docking Station" which can be used to inject CAOS Comands into the Game.
+
+### License ###
+The CAOS Console, and all it's Parts are Licensed under the [GPL3](https://gnu.org/licenses/gpl-3.0)!
+
+### How do I set it up? ###
+
+Just Download the Visual Studio Project and compile it with Visual Studio.
+
+### Usage ###
+1. Start your Game. ("Docking Station" or "Creatures 3")
+2. Start the Caos Console
+3. If No Error occures, Enter some CAOS Commands to see if it works… (i.e. http://creatures.wikia.com/wiki/C3/DS_CAOS_Codes
+
+### Advanced Usage ###
+If you Plan to run "Creatures 3" and "Docking Station" at the same Time, the Console will atache to the "Docking Station" Game.
+To prevent it from doing so, you can provide it the "Game Name" within the first comandline Parameter like this:
+```
+"CAOS Console.exe" "Creatures 3"
+```
